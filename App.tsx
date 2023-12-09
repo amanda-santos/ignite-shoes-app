c7e91da766c1399a9d39b2ca7b55253e400b819e
amanda-santos <amandasf0200@gmail.com>
2023-12-09 20:25:36 -0300

    chore: ➕ install OneSignal

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import OneSignal from "react-native-onesignal";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import { Routes } from './src/routes';
+import { Routes } from "./src/routes";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
+import { CartContextProvider } from "./src/contexts/CartContext";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+OneSignal.setAppId(process.env.EXPO_PUBLIC_ONE_SIGNAL_APP_ID || "");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });

1a0f49336dc0802f1039f6fd350ed548c0763887
amanda-santos <amandasf0200@gmail.com>
2023-12-11 19:32:14 -0300

    feat: use tags to identify user

--- App.tsx
+++ App.tsx
@@ -12,6 +12,7 @@ import { Routes } from "./src/routes";
 import { THEME } from "./src/theme";
 import { Loading } from "./src/components/Loading";
 import { CartContextProvider } from "./src/contexts/CartContext";
+import { tagUserEmailCreate } from "./src/notifications/notificationsTags";
 
 OneSignal.setAppId(process.env.EXPO_PUBLIC_ONE_SIGNAL_APP_ID || "");
 
@@ -25,6 +26,8 @@ OneSignal.setEmail("amanda@email.com");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserEmailCreate("amanda2@email.com");
 
   return (
     <NativeBaseProvider theme={THEME}>
